3c79e9e9b58e6d28331cada3f9b51c75a68acb9f
PasupulaS-Hexaware <109951310+PasupulaS-Hexaware@users.noreply.github.com>
2023-03-01 17:25:51 +0530

    Added Imapper in usingstudentcontrollerspecs.cs

--- dotnetwithmongodb/Code/dotnetwithmongodb.Test.Api/StudentControllerSpec/UsingStudentControllerSpec.cs
+++ dotnetwithmongodb/Code/dotnetwithmongodb.Test.Api/StudentControllerSpec/UsingStudentControllerSpec.cs
@@ -2,6 +2,7 @@ using NSubstitute;
 using dotnetwithmongodb.Test.Framework;
 using dotnetwithmongodb.Api.Controllers;
 using dotnetwithmongodb.BusinessServices.Interfaces;
+using AutoMapper;
 
 
 namespace dotnetwithmongodb.Test.Api.StudentControllerSpec
@@ -9,11 +10,12 @@ namespace dotnetwithmongodb.Test.Api.StudentControllerSpec
     public abstract class UsingStudentControllerSpec : SpecFor<StudentController>
     {
         protected IStudentService _studentService;
+        protected IMapper _mapper;
 
         public override void Context()
         {
             _studentService = Substitute.For<IStudentService>();
-            subject = new StudentController(_studentService);
+            subject = new StudentController(_studentService,_mapper);
 
         }
 
